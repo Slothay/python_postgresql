7b7ceaebd5c42d35a18534605e6c48ecbd26701f
Salla Kivistö <kivistosalla@gmail.com>
2025-02-07 16:49:15 +0200

    Refined user input for giving raise and inserting new records

--- python_to_postgres.py
+++ python_to_postgres.py
@@ -8,27 +8,39 @@ Choose from the menu what would you like to do. Press the corresponding number.
 1. Print table values
 2. Reset the database
 3. Insert new record
-4. Update Value(s)
+4. Give everyone a raise
 5. Delete Record
 '''
 
 def reset_table():
     cur.execute('DROP TABLE IF EXISTS employee')
     create_script = """ CREATE TABLE IF NOT EXISTS employee (
-                            id      int PRIMARY KEY,
+                            id      SERIAL PRIMARY KEY,
                             name    varchar(40) NOT NULL,
                             salary  int,
                             dept_id varchar(30)) """
     cur.execute(create_script)
 
 def insert_records():
-    insert_script = """INSERT INTO employee (id, name, salary, dept_id) VALUES(%s,%s,%s,%s)"""
-    insert_values = [(1, 'James', 12000, 'D1'), (2, 'Robin', 15000, 'D1'), (3, 'John', 20000, 'D2')]
+    keep_inserting_values = True
+    insert_values=[]
+    while keep_inserting_values:
+        name = input("Please give employee name:")
+        salary = input("Please give employee salary:")
+        dept_id = input("Please give department ID:")
+        insert_script = "INSERT INTO employee (name, salary, dept_id) VALUES(%s,%s,%s)"
+        insert_values.append((name,salary,dept_id))
+        continue_adding = input("Do you want to add another employee record? (Y/N):").upper()
+        if not continue_adding:
+            keep_inserting_values = False
     for record in insert_values:
         cur.execute(insert_script, record)
 
 def update_values():
-    update_script = 'UPDATE employee SET salary = salary + (salary*0.5)'
+    raise_to_give = float(input("How big of a raise you want to give in percentages?:"))
+    raise_to_give = raise_to_give*0.01
+    raise_record = str(raise_to_give)
+    update_script = f'UPDATE employee SET salary = salary + (salary*{raise_record})'
     cur.execute(update_script)
 
 def delete_record(delete_r):
@@ -41,7 +53,7 @@ def delete_record(delete_r):
             delete_record = (delete_r,)
             cur.execute(delete_script, delete_record)
     if i == 0:
-        print("There is no such employee name in the records.")
+        print("There is no such employee in the records.")
 
 def print_table():
     cur.execute('SELECT * FROM employee')
@@ -49,39 +61,36 @@ def print_table():
         print(record['name'], record['salary'])
 
 conn = None
-menu = ["0","1","2","3","4","5"]
 exit_program = False
 try:
     params = configurate()
-    with psycopg2.connect(
-        **params
-    ) as conn:
+    with psycopg2.connect(**params) as conn:
 
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
 
             while not exit_program:
                 print(MENU)
                 answer = input("What would you like to do? ")
-                try:
-                    if answer == "0":
+                match answer:
+                    case "0":
                         print("Goodbye!")
                         exit_program = True
-                    elif answer == "2":
+                    case "1":
+                        print_table()
+                    case "2":
                         reset_table()
                         print("Table reset.")
-                    elif answer == "3":
+                    case "3":
                         insert_records()
-                        print("Inserted values")
-                    elif answer == "4":
+                        print("Inserted values.")
+                    case "4":
                         update_values()
-                        print("Data updated.")
-                    elif answer == "1":
-                        print_table()
-                    elif answer == "5":
+                        print("Everyone gained raise.")
+                    case "5":
                         to_delete = input("Which employee record would you like to delete?:")
                         delete_record(to_delete)
-                except ValueError:
-                    print("Please choose valid option.")
+                    case _:
+                        print("Please choose a valid option.")
 
 except Exception as error:
     print(error)
